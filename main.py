b620e59305bd736ab98c7d8406c17b92656c878a
mrbear-zhiyi <Li18926423980@outlook.com>
2026-03-29 20:48:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Transfer learning from Alpha to cluster decay
+Author: MRBEAR
+Time：2026-1-30 00:54
+version：v2.6
+"""
+from utils.DNN_Data import *
+from utils.DNN_Train import *
+from utils.DNN_Transfer import *
+from utils.DNN_Visualization import *
+
+
+
+# prepare_data()
+# train_alpha_models()
+# train_combined_models_TL()
+# transfer_models()
+# Seed_evaluation_TL()
+# Batch_transfer_learning()
+# Seed_evaluation_TL(seed_list=[10122,1557,15602,17008,17173,17831, 18688,20123,21648, 22105,22249,25243,26556,26703, 26897,27780,3207,
+#                               35087,35598,38927,40575,40840,42013,42601, 51530,52761, 53117,56611,59470,59538,61601,62508,63315,75642,
+#                               76464,78488,79467,79722,80608,81723,81921, 82529,83080,86149,90709,90935,91865,9738,9934])
+# evaluate_TL_models()
+visualize_section()
+
+
+
+
